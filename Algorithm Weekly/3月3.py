88efe3eec15f7abc8eb840969a9366253be4c143
python <32657390+liyihanxx@users.noreply.github.com>
2019-03-01 17:39:56 +0800

    Update 3月3.py

--- Algorithm Weekly/3月3.py
+++ Algorithm Weekly/3月3.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def twoSum(self,nums,target):
+        n=len(nums)
+        for x in range(n):
+            for y in range(x+1,n):
+                if nums[y]  ==  target - nums[x]:
+                    return x,y
+                    break
+                else:
+                    continue
